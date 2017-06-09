ece8c986dbe88a9420cc042142f5d3177bfe7666
Jannis R <mail@jannisr.de>
2017-06-09 16:25:57 +0200

    format option

--- cli.js
+++ cli.js
@@ -3,6 +3,9 @@
 
 const minimist = require('minimist')
 const through = require('through2')
+const omit = require('lodash.omit')
+const ndjson = require('ndjson').stringify
+const csv = require('csv-write-stream')
 
 const getYoutubeMetadata = require('.')
 const pkg = require('./package.json')
@@ -14,8 +17,13 @@ if (argv.help || argv.h) {
 Usage:
     youtube-metadata <url>
 
+Options:
+    --format -f  Output format.
+                 Default: csv
+                 Available: csv,ndjson,url
+
 Examples:
-    youtube-metadata 'https://www.youtube.com/watch?v=rFtP7Xc_Fbo'
+    youtube-metadata --format url 'https://www.youtube.com/watch?v=rFtP7Xc_Fbo'
 \n`)
 	process.exit()
 }
@@ -31,12 +39,64 @@ const showError = function (err) {
 	process.exit(err.code || 1)
 }
 
-const formatter = through.obj((meta, _, cb) => {
-	cb(null, meta.webpage_url + '\n')
-})
-formatter
-.on('error', showError)
-.pipe(process.stdout)
+let formatter = process.stdout
+if (argv.format === 'url' || argv.f === 'url') {
+	formatter = through.obj((meta, _, cb) => {
+		cb(null, meta.webpage_url + '\n')
+	})
+	formatter
+	.on('error', showError)
+	.pipe(process.stdout)
+}
+else if (argv.format === 'ndjson' || argv.f === 'ndjson') {
+	const simplify = (meta, _, cb) => {
+		const simplified = omit(meta, [
+			'series', 'chapters', 'season_number', 'episode_number',
+			'_filename', 'ext',
+			'format', 'abr', 'acodec', 'vbr', 'vcodec', 'format_id', 'resolution',
+			'requested_formats',
+			'extractor_key', 'extractor'
+		])
+		for (let format of simplified.formats) delete format.http_headers
+		cb(null, simplified)
+	}
+
+	formatter = through.obj(simplify)
+	formatter
+	.on('error', showError)
+	.pipe(ndjson())
+	.on('error', showError)
+	.pipe(process.stdout)
+}
+else if (argv.format === 'csv' || argv.f === 'csv' || (!argv.format && !argv.f)) {
+	const simplify = (meta, _, cb) => cb(null, {
+		id: meta.id,
+		url: meta.webpage_url,
+		title: meta.fulltitle,
+		user: meta.uploader_id,
+		tags: meta.tags,
+		categories: meta.categories,
+		duration: meta.duration,
+		thumbnail: meta.thumbnail,
+		resolution: meta.width + 'x' + meta.height,
+
+		playlist: meta.playlist_id,
+		index: meta.playlist_index,
+
+		views: meta.view_count,
+		rating: meta.average_rating,
+		likes: meta.like_count,
+		dislikes: meta.dislike_count,
+	})
+
+	formatter = through.obj(simplify)
+	formatter
+	.on('error', showError)
+	.pipe(csv())
+	.on('error', showError)
+	.pipe(process.stdout)
+}
+else showError('Invalid format.')
 
 const url = argv._[0]
 if (!url) showError('Missing URL.')
